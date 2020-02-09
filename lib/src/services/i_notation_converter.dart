950369691b9023cfa44e705fa398c4467d0c5ecb
James Leahy <defuncart@gmail.com>
2020-02-09 13:58:36 +0100

    Add INotationConverter and CustomNotationConverter.

---
+++ lib/src/services/i_notation_converter.dart
@@ -0,0 +1,7 @@
+import '../models/move.dart';
+
+/// An interface which different notations (kifu, custom) can implement
+abstract class INotationConverter {
+  /// Converts a game from text into a list of moves
+  List<Move> movesFromFile(String file);
+}
