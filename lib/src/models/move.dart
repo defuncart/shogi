d2328f8a1701bbfe00e05f7356cd822dac486293
James Leahy <defuncart@gmail.com>
2020-02-24 18:53:31 +0100

    Add isPromotion, isCapture, isDrop to Move.

--- lib/src/models/move.dart
+++ lib/src/models/move.dart
@@ -14,21 +14,40 @@ class Move {
   final PieceType piece;
 
   /// The original position
+  ///
+  /// `null` if piece is dropped onto the board
   final Position from;
 
   /// The new position
   final Position to;
+
+  /// Whether the move promotes the piece
+  final bool isPromotion;
+
+  /// Whether the move captures an oppenent's piece
+  final bool isCapture;
+
+  /// Whether the move drops a piece onto the board
+  final bool isDrop;
 
   const Move({
     @required this.player,
     @required this.piece,
     @required this.from,
     @required this.to,
+    this.isPromotion = false,
+    this.isCapture = false,
+    this.isDrop = false,
   })  : assert(player != null),
         assert(piece != null),
-        assert(from != null),
-        assert(to != null);
+        assert(to != null),
+        assert(isPromotion != null),
+        assert(isCapture != null),
+        assert(isDrop != null),
+        assert(!(isPromotion && isDrop)), //a move cannot have isDrop and isPromotion
+        assert(isDrop ? from == null : from != null); //if isDrop, from should not be null otherwise should be null
 
   @override
-  String toString() => '${DartUtils.describeEnum(player)} with ${DartUtils.describeEnum(piece)} from $from to $to';
+  String toString() =>
+      '${DartUtils.describeEnum(player)} with ${DartUtils.describeEnum(piece)} from $from to $to. isPromotion: $isPromotion, isCapture: $isCapture, isDrop: $isDrop.';
 }
