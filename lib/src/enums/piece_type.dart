011b3f26b535e8747f1c82246f035a8fae048379
James Leahy <defuncart@gmail.com>
2020-02-26 17:49:01 +0100

    Added PieceType extensions. Updated dart sdk to 2.6.

--- lib/src/enums/piece_type.dart
+++ lib/src/enums/piece_type.dart
@@ -15,3 +15,32 @@ enum PieceType {
   lancePromoted,
   pawnPromoted,
 }
+
+/// A class of extensions for PieceType
+extension PieceTypeExtensions on PieceType {
+  /// A list of piece types which can be promoted
+  static const piecesWhichCanBePromoted = const [
+    PieceType.pawn,
+    PieceType.lance,
+    PieceType.knight,
+    PieceType.silver,
+    PieceType.bishop,
+    PieceType.rook,
+  ];
+
+  /// A map of piece type and their promoted equivalents
+  static const convertPieceTypeToPromotedPieceType = const {
+    PieceType.pawn: PieceType.pawnPromoted,
+    PieceType.lance: PieceType.lancePromoted,
+    PieceType.knight: PieceType.knightPromoted,
+    PieceType.silver: PieceType.silverPromoted,
+    PieceType.bishop: PieceType.bishopPromoted,
+    PieceType.rook: PieceType.rookPromoted,
+  };
+
+  /// Whether the piece type can be promoted
+  bool get canBePromoted => piecesWhichCanBePromoted.contains(this);
+
+  /// Returns a promoted equivalent for the piece type. Returns `null` if the piece type cannot be promoted.
+  PieceType get promoted => this.canBePromoted ? convertPieceTypeToPromotedPieceType[this] : null;
+}
