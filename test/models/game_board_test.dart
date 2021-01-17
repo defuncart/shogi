a1508d4d8598d506342f43c3b350bf810bb5ebe1
James Leahy <defuncart@gmail.com>
2021-01-17 14:29:25 +0100

    Add equality checks to GameBoard.

--- test/models/game_board_test.dart
+++ test/models/game_board_test.dart
@@ -17,4 +17,54 @@ void main() {
     expect(gameBoard.sentePiecesInHand, isEmpty);
     expect(gameBoard.gotePiecesInHand, isEmpty);
   });
+
+  test('Equality', () {
+    final gameBoard1 = GameBoard(boardPieces: [
+      BoardPiece(
+        pieceType: PieceType.pawn,
+        player: PlayerType.sente,
+        position: Position.fromString('97'),
+      ),
+      BoardPiece(
+        pieceType: PieceType.pawn,
+        player: PlayerType.gote,
+        position: Position.fromString('13'),
+      ),
+      BoardPiece(
+        pieceType: PieceType.pawn,
+        player: PlayerType.sente,
+        position: null,
+      ),
+      BoardPiece(
+        pieceType: PieceType.pawn,
+        player: PlayerType.gote,
+        position: null,
+      ),
+    ]);
+    final gameBoard2 = GameBoard(boardPieces: [
+      BoardPiece(
+        pieceType: PieceType.pawn,
+        player: PlayerType.sente,
+        position: Position.fromString('97'),
+      ),
+      BoardPiece(
+        pieceType: PieceType.pawn,
+        player: PlayerType.gote,
+        position: Position.fromString('13'),
+      ),
+      BoardPiece(
+        pieceType: PieceType.pawn,
+        player: PlayerType.sente,
+        position: null,
+      ),
+      BoardPiece(
+        pieceType: PieceType.pawn,
+        player: PlayerType.gote,
+        position: null,
+      ),
+    ]);
+    expect(gameBoard1, isNotNull);
+    expect(gameBoard2, isNotNull);
+    expect(gameBoard1, gameBoard2);
+  });
 }
