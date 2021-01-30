ced026fd9daa6b986d37979bbd4fa5489a7f8492
James Leahy <defuncart@gmail.com>
2021-01-30 12:21:40 +0100

    GameParser: pass initialBoard to KifConverter.

--- lib/src/services/parser/game_parser.dart
+++ lib/src/services/parser/game_parser.dart
@@ -6,7 +6,8 @@ abstract class GameParser {
   /// Parses a game from a text file
   static ParsedGame fromKif(String file) {
     final initialBoard = BODConverter.bodToGameBoard(file);
-    final moves = KIFNotationConverter().movesFromFile(file);
+    final moves =
+        KIFNotationConverter().movesFromFile(file, initialBoard: initialBoard);
     final winner = KIFNotationConverter().determineWinner(file);
 
     if (initialBoard != null && moves != null && moves.isNotEmpty) {
