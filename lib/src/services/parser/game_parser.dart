a952fde5f61c6d4ff4c36abe1b2bceb272e720f6
James Leahy <defuncart@gmail.com>
2021-01-30 09:15:57 +0100

    Add GameParser.

---
+++ lib/src/services/parser/game_parser.dart
@@ -0,0 +1,22 @@
+import '../../models/parsed_game.dart';
+import '../board_notation/bod_converter.dart';
+import '../move_notation/kif_notation_converter.dart';
+
+abstract class GameParser {
+  /// Parses a game from a text file
+  static ParsedGame fromKif(String file) {
+    final initialBoard = BODConverter.bodToGameBoard(file);
+    final moves = KIFNotationConverter().movesFromFile(file);
+    final winner = KIFNotationConverter().determineWinner(file);
+
+    if (initialBoard == null || moves == null) {
+      return null;
+    }
+
+    return ParsedGame(
+      initalGameBoard: initialBoard,
+      moves: moves,
+      winner: winner,
+    );
+  }
+}
