e4a90de42a40bfbe5e0ea154ea065e6f56c7f4c5
James Leahy <defuncart@gmail.com>
2021-01-30 09:13:04 +0100

    ShogiUtils add bodStringToGameBoard.

--- lib/src/utils/shogi_utils.dart
+++ lib/src/utils/shogi_utils.dart
@@ -4,6 +4,7 @@ import '../enums/player_type.dart';
 import '../models/board_piece.dart';
 import '../models/game_board.dart';
 import '../models/position.dart';
+import '../services/board_notation/bod_converter.dart';
 import '../services/board_notation/sfen_converter.dart';
 
 /// A class of utils methods used when constructing a shogi board
@@ -11,6 +12,10 @@ class ShogiUtils {
   /// Converts a SFEN ascii string into a `GameBoard`
   static GameBoard sfenStringToGameBoard(String string) =>
       SFENConverter.sfenToGameBoard(string);
+
+  /// Converts a BOD string into a `GameBoard`
+  static GameBoard bodStringToGameBoard(String string) =>
+      BODConverter.bodToGameBoard(string);
 
   /// Flips a list of board pieces to another player
   ///
